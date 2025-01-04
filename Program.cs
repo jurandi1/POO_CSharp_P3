52c2d260fe40e6cd61ac9f37d20da5f0565ac29a
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 16:37:13 -0300

    Membros estáticos P1 - métodos na própria classe do programa

--- Program.cs
+++ Program.cs
@@ -1,7 +1,38 @@
-﻿internal class Program
+﻿using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Globalization;
+
+internal class Program
 {
+    static double Pi = 3.14;
+
     private static void Main(string[] args)
     {
+        //        Problema exemplo
+        //Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+        //quanto seria o valor de uma circunferência e do volume de uma esfera
+        //para um raio daquele valor. Informar também o valor de PI com duas
+        //casas decimais.
 
+        Console.Write("Entre o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double circ = Circunferencia(raio);
+        double volume = Volume(raio);
+
+        Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture)); 
     }
+
+    static double Circunferencia(double r)
+    {
+        return 2.0 * Pi * r; 
+    }
+
+    static double Volume(double r)
+    {
+        return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+    }
+
 }
