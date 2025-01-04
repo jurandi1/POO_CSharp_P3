7357dea02e46e8d13bbc84aafc43fd6b9ee2be12
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 16:51:53 -0300

    Membros estáticos P3 - classe Calculadora com método estático

--- Program.cs
+++ Program.cs
@@ -14,17 +14,16 @@ internal class Program
         //para um raio daquele valor. Informar também o valor de PI com duas
         //casas decimais.
 
-        Calculadora calc = new Calculadora(); 
 
         Console.Write("Entre o valor do raio: ");
         double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        double circ = calc.Circunferencia(raio);
-        double volume = calc.Volume(raio);
+        double circ = Calculadora.Circunferencia(raio);
+        double volume = Calculadora.Volume(raio);
 
         Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
         Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-        Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture)); 
+        Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)); 
     }
 
 }
